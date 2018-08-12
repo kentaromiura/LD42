0f1132d102519d94086ff1056b4f76ccf98b566d
sideroad <sideroad.jp@gmail.com>
2018-08-12 11:21:13 +0900

    Refactoring add more callbacks

--- src/p2p.js
+++ src/p2p.js
@@ -2,18 +2,26 @@ import Peer from 'peerjs';
 
 export default () =>
   new Promise(resolve => {
-    const cbs = [];
+    const cbs = {
+      'message': [],
+      'disconnected': [],
+      'new-attendee': [],
+      'leave-attendee': []
+    };
     const conns = [];
     const register = (conn) => {
       conns.push(conn);
       conn.on('data', (data) => {
-        cbs.forEach(cb => cb(JSON.parse(data)));
+        cbs.message.forEach(cb => cb(JSON.parse(data)), conn.peer);
+      });
+      conn.on('close', () => {
+        cbs['leave-attendee'].forEach(cb => cb({
+          id: conn.peer
+        }));
       });
     };
     var peer = new Peer();
     peer.on('open', (id) => {
-      console.log('My peer ID is: ' + id);
-
       fetch(`https://gamebroker.herokuapp.com/ld42/join?id=${id}`)
         .then(res => res.json())
         .then((res) => {
@@ -22,15 +30,25 @@ export default () =>
               register(peer.connect(client))
             )
           }
-          peer.on('connection', (conn) => register(conn));
+          peer.on('disconnected', (conn) => {
+            cbs.disconnected.forEach(cb => cb(conn.peer));
+          });
+          peer.on('connection', (conn) => {
+            register(conn);
+            cbs['new-attendee'].forEach(cb => cb(conn.peer));
+          });
           resolve({
+            id: id,
+            size: () => conns.length,
+            disconnect: () => peer.disconnect(),
+            to: (to, data) => (conns.find(conn => conn.peer === to) || {send: () => {}}).send(JSON.stringify(data)),
             emit: (data) => {
               conns.forEach(conn => {
                 conn.send(JSON.stringify(data));
               });
             },
-            cb: (cb) => {
-              cbs.push(cb);
+            on: (type, cb) => {
+              cbs[type].push(cb);
             }
           });
         });
