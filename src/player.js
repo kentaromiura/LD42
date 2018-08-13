2566a6c4ea99cefc7d41c03068ba148983a42f3c
Cristian Carlesso <kentaromiura@gmail.com>
2018-08-13 21:14:57 +0900

    adding shield protection before death

--- src/player.js
+++ src/player.js
@@ -21,17 +21,61 @@ const clamp = (point, boundary) => {
 };
 
 export default class Player extends GameObject {
-  constructor(sprite, state, x = 0, y = 0, w = 128, h = 128) {
+  constructor(sprite, spritepowered, state, x = 0, y = 0, w = 128, h = 128) {
     super(x, y, w, h);
     this.nextShotIn = 15;
     this.currentShotSpeed = 15;
     this.state = state;
     this.sprite = sprite;
+    this.basesprite = sprite;
+
+    this.powered = spritepowered;
+    this.powered.animationSpeed = 1 / 5;
+
+    this.isPowered = false;
     sprite.anchor.x = 0.5;
     sprite.anchor.y = 0.5;
+    this.powered.anchor.x = 0.5;
+    this.powered.anchor.y = 0.5;
     sprite.position = vector(x, y);
+
     sprite.width = w;
     sprite.height = h;
+    this.powered.width = w;
+    this.powered.height = h;
+  }
+
+  powerUp() {
+    this.isPowered = true;
+    const { x, y } = this.sprite.position;
+    Event.fire(EVENTS.EXIT_STAGE, { sprite: this.sprite });
+
+    this.sprite = this.powered;
+    this.sprite.position.x = x;
+    this.sprite.position.y = y;
+    Event.fire(EVENTS.ENTER_STAGE, { sprite: this.sprite });
+    this.sprite.play();
+  }
+
+  hit() {
+    let isAlive = true;
+    if (!this.isPowered) {
+      isAlive = false;
+      this.sprite.destroy();
+    } else {
+      this.powerDown();
+    }
+    return isAlive;
+  }
+
+  powerDown() {
+    this.isPowered = false;
+    const { x, y } = this.sprite.position;
+    Event.fire(EVENTS.EXIT_STAGE, { sprite: this.sprite });
+    this.sprite = this.basesprite;
+    this.sprite.position.x = x;
+    this.sprite.position.y = y;
+    Event.fire(EVENTS.ENTER_STAGE, { sprite: this.sprite });
   }
 
   updatePosition() {
